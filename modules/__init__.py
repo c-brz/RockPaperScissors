e8de100e71cadab480dca4021ebd0758f1fe468a
c-brz <christineb07@outlook.com.gr>
2025-10-01 11:04:45 +0300

    Wipe main branch clean

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-# modules/__init__.py
-from .hand_visualizations import HandVisualRepresentations
-
-__all__ = ['HandVisualRepresentations']
--- setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py (in project root)
-from setuptools import setup, find_packages
-
-setup(
-    name="RockPaperScissors",
-    version="0.1.0",
-    packages=find_packages(),
-)
